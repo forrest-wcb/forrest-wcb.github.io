60a227005322e732b624452c9241b9a114ea9596
forrest-wcb <2410485087@qq.com>
2025-01-12 12:24:51 +0800

    Site updated: 2025-01-12 12:24:50

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="小站发金条啦～",clearTimeout(titleTime)):(document.title="欢迎回来！",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
